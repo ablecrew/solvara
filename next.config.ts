960ee9a2b9f25982f67045d536d258184a34a2f4
ablecrew <teddy.dande4@gmail.com>
2026-07-08 17:17:23 +0300

    Prepare for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: "export",
   images: {
-      remotePatterns: [
-        { protocol: "https", hostname: "res.cloudinary.com" },
-      ],
-    },
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
